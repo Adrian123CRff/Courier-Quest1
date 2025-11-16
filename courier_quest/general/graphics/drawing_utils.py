7da2f8a293a1a8a148fd61949fb95a0070a0d617
Andy <andy.badilla.herrera@est.una.ac.cr>
2025-11-16 12:32:32 -0600

    Agregado clase de drawing_utils

---
+++ courier_quest/general/graphics/drawing_utils.py
@@ -0,0 +1,15 @@
+#drawing_utils.py
+"""
+Drawing utilities - Helper functions for drawing rectangles
+"""
+import arcade
+
+
+def _draw_rect_lrbt_filled(left: float, right: float, bottom: float, top: float, color) -> None:
+    """Draw a filled rectangle with left, right, bottom, top coordinates."""
+    arcade.draw_lrbt_rectangle_filled(left, right, bottom, top, color)
+
+
+def _draw_rect_lrbt_outline(left: float, right: float, bottom: float, top: float, color, line_width: int = 1) -> None:
+    """Draw an outlined rectangle with left, right, bottom, top coordinates."""
+    arcade.draw_lrbt_rectangle_outline(left, right, bottom, top, color, line_width)
